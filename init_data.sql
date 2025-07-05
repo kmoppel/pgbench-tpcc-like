facb7d901f4ecef4b5c751abaaaf31b2174eff02
Kaarel Moppel <kaarel.moppel@gmail.com>
2025-07-05 19:32:04 +0300

    WIP data init

---
+++ init_data.sql
@@ -0,0 +1,106 @@
+INSERT INTO item (i_im_id, i_name, i_price, i_data)
+SELECT
+  random()*1e4,
+  tdgen.random_text(14, 24),
+  tdgen.random_float(1, 100),
+  CASE
+    WHEN random() < 0.1 THEN tdgen.random_text(10, 20) || ' ORIGINAL ' || tdgen.random_text(10, 20)
+    ELSE tdgen.random_text(26, 50)
+  END CASE
+FROM generate_series(1, 1e5) gs ;
+
+
+INSERT INTO warehouse (
+  w_name,
+  w_street_1,
+  w_street_2,
+  w_city,
+  w_state,
+  w_zip,
+  w_tax,
+  w_ytd
+)
+SELECT
+  'wh-' || tdgen.random_text(3,7),
+  'str-' || tdgen.random_text(6,16),
+  'str-' || tdgen.random_text(6,16),
+  'ct-' || tdgen.random_text(6,16),
+  tdgen.random_text(2, 2, 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'),
+  tdgen.random_text(5, 5, '0123456789'), -- W_ZIP generated accord ing to Clau se 4.3.2.7
+  tdgen.random_float(0, 0.2),
+  300000
+;
+
+INSERT INTO stock (s_w_id, s_i_id, s_quantity, s_dist_01, s_dist_02, s_dist_03, s_dist_04, s_dist_05, s_dist_06,
+    s_dist_07, s_dist_08, s_dist_09, s_dist_10, s_ytd, s_order_cnt, s_remote_cnt, s_data)
+SELECT
+  (select max(w_id) from warehouse),
+  ((select max(w_id) from warehouse) - 1) * 1e5 + gs,
+  tdgen.random_int(10, 100),
+  tdgen.random_text(24, 24),
+  tdgen.random_text(24, 24),
+  tdgen.random_text(24, 24),
+  tdgen.random_text(24, 24),
+  tdgen.random_text(24, 24),
+  tdgen.random_text(24, 24),
+  tdgen.random_text(24, 24),
+  tdgen.random_text(24, 24),
+  tdgen.random_text(24, 24),
+  tdgen.random_text(24, 24),
+  0,
+  0,
+  0,
+  CASE
+    WHEN random() < 0.1 THEN tdgen.random_text(10, 20) || ' ORIGINAL ' || tdgen.random_text(10, 20)
+    ELSE tdgen.random_text(26, 50)
+  END CASE
+FROM generate_series(1, 1e5) gs ;
+
+
+
+
+
+INSERT INTO district (
+  d_id, d_w_id, d_name, d_street_1, d_street_2, d_city, d_state, d_zip, d_tax, d_ytd, d_next_o_id
+)
+SELECT
+  gs,
+  (select max(w_id) from warehouse),
+  tdgen.random_text(6,10, 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'),
+  'str-' || tdgen.random_text(6,16),
+  'str-' || tdgen.random_text(6,16),
+  'ct-' || tdgen.random_text(6,16),
+  tdgen.random_text(2, 2, 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'),
+  tdgen.random_text(5, 5, '0123456789'), -- W_ZIP generated accord ing to Clause 4.3.2.7
+  tdgen.random_float(0, 0.2),
+  30000,
+  3001
+FROM generate_series(1, 10) gs ;
+
+
+
+INSERT INTO customer (c_id, c_d_id, c_w_id, c_first, c_middle, c_last, c_street_1, c_street_2, c_city, c_state, c_zip,
+    c_phone, c_credit, c_credit_lim, c_discount, c_balance, c_ytd_payment, c_payment_cnt, c_delivery_cnt, c_data)
+SELECT
+  ((select max(w_id) from warehouse) - 1) * 3000 + gsc,
+  ((select max(w_id) from warehouse) - 1) * 10 + gsd,
+  (select max(w_id) from warehouse),
+  tdgen.random_text(8, 16),
+  'OE',
+  'cust-' || (random()*1000)::int::text, -- TODO NURand (255,0,999)
+  'str-' || tdgen.random_text(6,16),
+  'str-' || tdgen.random_text(6,16),
+  'ct-' || tdgen.random_text(6,16),
+  tdgen.random_text(2, 2, 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'),
+  tdgen.random_text(5, 5, '0123456789'), -- W_ZIP generated accord ing to Clause 4.3.2.7
+  tdgen.random_text(16, 16, '0123456789'),
+  CASE WHEN gsc % 10 = 0 THEN 'BC' ELSE 'GC' END CASE,
+  50000,
+  tdgen.random_float(0, 0.5),
+  -10,
+  10,
+  1,
+  0,
+  repeat(gen_random_uuid()::text||gen_random_uuid()::text, 5)
+-- For each row in the DISTRICT table 3000 rows in the CUSTOMER table
+FROM generate_series(1, 3000) gsc, generate_series(1, 10) gsd;
